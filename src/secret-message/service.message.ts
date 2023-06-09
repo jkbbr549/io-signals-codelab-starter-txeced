7e2c3f1e3b2df3da59d0dc6932d8f8c34ab630da
jkbbr549 <jkbbr549@yahoo.com>
2023-06-09 13:17:22 -0500

    Initial commit

---
+++ src/secret-message/service.message.ts
@@ -0,0 +1,39 @@
+import { computed, Injectable, signal } from '@angular/core';
+import { CipherKey, CipherService } from '../cipher/service.cipher';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  // TODO(1): Define your first signal()
+  superSecretMessage = 'Angular Signals are in developer preview in v16 today!';
+
+  // TODO(1): Define your first signal()
+  secretMessage = computed(() =>
+    this.translateMessage(this.superSecretMessage, this.cipher.cipher())
+  );
+  // TODO(2): Define your first computed()
+  solvedMessage = this.translateMessage(
+    this.secretMessage(),
+    this.cipher.decodedCipher()
+  );
+
+  constructor(private cipher: CipherService) {}
+
+  translateMessage(code: string, ciph: CipherKey[]): string {
+    let encoded = '';
+
+    code.split('').forEach((char) => {
+      let isUpperCase = false;
+      if (char == char.toUpperCase()) {
+        isUpperCase = true;
+      }
+
+      let value = ciph.find((el) => el.key === char.toLowerCase())?.value;
+      let newChar = value ? value : char;
+
+      encoded += isUpperCase ? newChar.toUpperCase() : newChar;
+    });
+    return encoded;
+  }
+}
